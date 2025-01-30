7eded07ebc28dcde6f5933ae7085676985f200b5
Atul Rawat <atulrawat@Atuls-MacBook-Pro.local>
2025-01-30 12:56:02 +0530

    I think it is good

--- src/expences.js
+++ src/expences.js
@@ -1,10 +1,8 @@
-const numbers = [12.5, 3.45, 1.75, 2.3];
-
 const sum = (nums) => nums.reduce((num1, num2) => num1 + num2);
 
 class Budget {
   #entries;
-  constructor(entries) {
+  constructor(...entries) {
     this.#entries = entries;
   }
 
@@ -18,7 +16,7 @@ class Budget {
 }
 
 const main = () => {
-  const budget = new Budget(numbers);
+  const budget = new Budget(12.5, 3.45, 1.75, 2.3);
 
   console.log(budget.display());
 };
